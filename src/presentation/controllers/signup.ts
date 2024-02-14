fb46cd8de737a822ddae09195d7b1a4b93f471e0
decker12w <josemaia.comp@gmail.com>
2024-02-14 15:31:01 -0300

    chore: generic3

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,4 +1,5 @@
 export class SignUpController {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
   handle(httpRequest: any): any {
     return {
       statusCode: 400,
