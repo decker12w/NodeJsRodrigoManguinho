00974f20fac58ccf85352b0aa71e8c80d48455cd
decker12w <josemaia.comp@gmail.com>
2024-02-14 18:01:42 -0300

    test: ensure SignUpController return 400 if no passwordConfirmation  is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -48,4 +48,22 @@ describe("SignUp Controller", () => {
     expect(httpReponse.statusCode).toBe(400);
     expect(httpReponse.body).toEqual(new MissingParamError("password"));
   });
+
+  test("Should return 400 if no passwordConfirmation is provided", () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@example.com",
+        password: "any_password",
+      },
+    };
+
+    const httpReponse = sut.handle(httpRequest);
+    expect(httpReponse.statusCode).toBe(400);
+    expect(httpReponse.body).toEqual(
+      new MissingParamError("passwordConfirmation"),
+    );
+  });
 });
