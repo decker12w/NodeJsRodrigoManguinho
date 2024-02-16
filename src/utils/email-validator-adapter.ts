805a15edfc073c4a55fca29cdb86932439759dee
decker12w <josemaia.comp@gmail.com>
2024-02-16 15:50:38 -0300

    refactor: change eslint rule

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -2,7 +2,6 @@ import validator from "validator";
 import { EmailValidator } from "../presentation/protocols/email-validator";
 
 export class EmailValidatorAdapter implements EmailValidator {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   isValid(email: string): boolean {
     return validator.isEmail(email);
   }
